ed38eb61129ff1cd3e0df0dc30c80210dfb09689
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-06 19:57:58 -0300

    Seção de planos finalizada

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import Header from "../../components/Header/Header";
-import './sobre.css';
+import mockup from '../../assets/img/mockup.png';
 import prototipo1 from '../../assets/img/prototipo1.png';
 import prototipo2 from '../../assets/img/prototipo2.png';
-import mockup from '../../assets/img/mockup.png';
-import { User, Users } from 'lucide-react';
+import Header from "../../components/Header/Header";
 import SobreCard from "../../components/SobreCard/SobreCard";
+import './sobre.css';
 
 export default function Sobre() {
-    return(
+    return (
         <>
             <Header thisPage="sobre" />
             <div className="container-sobre">
                 <section className="first-section-sobre">
                     <div className="left-container-sobre">
                         <h1>Bem-vindo à revolução dos vídeos!</h1>
-                        <p>Somos uma startup com o objetivo de facilitar a produção de conteúdo em larga escala com qualidade profissional, 
+                        <p>Somos uma startup com o objetivo de facilitar a produção de conteúdo em larga escala com qualidade profissional,
                             capaz de atingir uma audiência de milhões de seguidores.
                         </p>
                         <button>Baixe o app</button>
@@ -30,19 +29,17 @@ export default function Sobre() {
                     <h2>Planos</h2>
                     <div className="plans-container">
                         <SobreCard title="Individual" buttonText="Cadastrar" type="individual">
-                            <User size={24} />
                             <ul>
-                                <li>1 Usuário</li>
-                                <li>10 vídeos R$15</li>
+                                <li className="users-information">1 Usuário</li>
+                                <li className='videos-information'>10 vídeos ,<b>R$15</b></li>
                             </ul>
                         </SobreCard>
                         <SobreCard title="Profissional - Times" buttonText="Cadastrar" type="professional">
-                            <Users size={24} />
                             <ul>
-                                <li>1-10 Usuários</li>
-                                <li>Vídeos Ilimitados R$40</li>
-                                <li>+10 Usuários</li>
-                                <li>Vídeos Ilimitados R$20</li>
+                                <li className="users-information">1-10 Usuários</li>
+                                <li className='videos-information'>Vídeos Ilimitados <b>R$40</b></li>
+                                <li className="users-information">+10 Usuários</li>
+                                <li className='videos-information'>Vídeos Ilimitados <b>R$20</b></li>
                             </ul>
                         </SobreCard>
                         <SobreCard title="Corporativo" buttonText="Entre em contato" type="corporate">
@@ -51,7 +48,7 @@ export default function Sobre() {
                     </div>
                 </section>
             </div>
-           
+
         </>
     )
 }
