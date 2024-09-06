a80c7c0ec6670ca6ee9b7b7776687dfd0df99783
Rafael Duarte <rafa.duartef@outlook.com>
2024-09-06 11:49:42 -0300

    Esqueleto para desenvolvimento das pages finalizada: header

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import './sobre.css';
 
 export default function Sobre() {
     return(
         <>
             <Header thisPage="sobre" />
+            <div className="container">
+
+            </div>
         </>
     )
 }
